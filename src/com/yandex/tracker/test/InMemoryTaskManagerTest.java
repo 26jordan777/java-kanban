34b77393f03794e1ebc0b45afbd09a32318ba328
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2024-12-19 21:27:30 +0300

    Изменение тетÐа InMemoryTaskManagerTest

--- src/com/yandex/tracker/test/InMemoryTaskManagerTest.java
+++ src/com/yandex/tracker/test/InMemoryTaskManagerTest.java
@@ -3,6 +3,8 @@ package com.yandex.tracker.test;
 import com.yandex.tracker.model.Epic;
 import com.yandex.tracker.model.Subtask;
 import com.yandex.tracker.model.Task;
+import com.yandex.tracker.model.Status;
+import com.yandex.tracker.model.TaskType;
 import com.yandex.tracker.service.InMemoryTaskManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +13,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class InMemoryTaskManagerTest {
-    // Патимат, привет! извиняюсь, почему-то не загрузились данные в классы,
-    // сами классы загрузились, но без дынных.
     private InMemoryTaskManager taskManager;
 
     @BeforeEach
@@ -22,7 +22,7 @@ public class InMemoryTaskManagerTest {
 
     @Test
     public void testCreateAndGetTask() {
-        Task task = new Task("Test Task", "Test Description");
+        Task task = new Task(1, TaskType.TASK, "Test Task", Status.NEW, "Test Description");
         taskManager.createTask(task);
 
         Task retrievedTask = taskManager.getTask(task.getId());
@@ -31,8 +31,10 @@ public class InMemoryTaskManagerTest {
 
     @Test
     public void testCreateAndGetSubtask() {
-        Epic epic = taskManager.createEpic(new Epic("Epic Name", "Epic Description"));
-        Subtask subtask = new Subtask(0, "Test Subtask", "Test Subtask Description", epic.getId());
+        Epic epic = new Epic(2, TaskType.EPIC, "Epic Name", Status.NEW, "Epic Description");
+        taskManager.createEpic(epic);
+
+        Subtask subtask = new Subtask(3, TaskType.SUBTASK, "Test Subtask", Status.NEW, "Test Subtask Description", epic.getId());
         taskManager.createSubtask(subtask);
 
         Subtask retrievedSubtask = taskManager.getSubtask(subtask.getId());
@@ -41,8 +43,8 @@ public class InMemoryTaskManagerTest {
 
     @Test
     public void testHistory() {
-        Task task1 = taskManager.createTask(new Task("Task 1", "Description 1"));
-        Task task2 = taskManager.createTask(new Task("Task 2", "Description 2"));
+        Task task1 = taskManager.createTask(new Task(4, TaskType.TASK, "Task 1", Status.NEW, "Description 1"));
+        Task task2 = taskManager.createTask(new Task(5, TaskType.TASK, "Task 2", Status.NEW, "Description 2"));
 
         taskManager.getTask(task1.getId());
         taskManager.getTask(task2.getId());
@@ -56,15 +58,14 @@ public class InMemoryTaskManagerTest {
     @Test
     public void testHistoryLimit() {
         for (int i = 0; i < 12; i++) {
-            taskManager.createTask(new Task("Task " + i, "Description " + i));
+            taskManager.createTask(new Task(i + 6, TaskType.TASK, "Task " + i, Status.NEW, "Description " + i));
         }
 
         for (int i = 0; i < 12; i++) {
-            taskManager.getTask(i);
+            taskManager.getTask(i + 6);
         }
 
         List<Task> history = taskManager.getHistory();
-        Assertions.assertEquals(10, history.size());
+        Assertions.assertEquals(10, history.size(), "History should contain only the last 10 tasks");
     }
-
 }
