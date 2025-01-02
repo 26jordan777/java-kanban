f28389afe8e154bfb9a68c4d952fcbdf4d3b1f69
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-01-02 21:12:41 +0300

    Изменения ьтестов

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -27,8 +27,8 @@ public class FileBackedTaskManagerTest  {
         taskManager.createTask(new Task(1, TaskType.TASK, "Task1", Status.NEW, "Description task1",
                 Duration.ofMinutes(30), LocalDateTime.now()));
 
-        taskManager.createEpic(new Epic(2, TaskType.EPIC, "Epic1", Status.NEW, "Description epic1"));
-
+        Epic epic = new Epic(2, TaskType.EPIC, "Epic1", Status.NEW, "Description epic1");
+        taskManager.createEpic(epic);
 
         taskManager.createSubtask(new Subtask(3, TaskType.SUBTASK, "Subtask1", Status.NEW,
                 "Description subtask1", epic.getId(),
