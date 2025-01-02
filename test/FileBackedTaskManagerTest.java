3699d5895cde46ccfaed81fb10f51edfcda521b2
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-01-03 01:16:21 +0300

    ИзÐменение taskFromString

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FileBackedTaskManagerTest extends TaskManagerTest {
+public class FileBackedTaskManagerTest  {
 
     @Test
     protected FileBackedTaskManager createTaskManager() {
