14bcdfb8f5ceaa88146152e020e9f381a42d1f40
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-01-03 01:05:15 +0300

    ИзÐменение taskFromString

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -28,7 +28,7 @@ public class FileBackedTaskManagerTest  {
         taskManager.createTask(new Task(1, TaskType.TASK, "Task1", Status.NEW, "Description task1",
                 Duration.ofMinutes(30), LocalDateTime.now()));
 
-        Epic epic = new Epic(2, TaskType.EPIC, "Epic1", Status.NEW, "Description epic1");
+       taskManager.createEpic(new Epic(2, TaskType.EPIC, "Epic1", Status.NEW, "Description epic1"));
         taskManager.createEpic(epic);
 
 
